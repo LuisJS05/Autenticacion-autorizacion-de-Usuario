c8e5c5156490cee75e1180439d047fadc2815380
Luis Ismael <silvest097@gmail.com>
2025-10-03 02:39:33 -0300

    proyecto actualizado

--- src/main/java/com/project_ecommerce/backecom/configuration/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/project_ecommerce/backecom/configuration/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.project_ecommerce.backecom.configuration;
 
 
+import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,13 +9,11 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException, IOException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException, IOException, java.io.IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 
--- src/main/java/com/project_ecommerce/backecom/dao/RoleDao.java
+++ src/main/java/com/project_ecommerce/backecom/dao/RoleDao.java
@@ -1,7 +1,6 @@
 package com.project_ecommerce.backecom.dao;
 
 
-
 import com.project_ecommerce.backecom.entity.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
